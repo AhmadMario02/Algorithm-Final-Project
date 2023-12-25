1917320e5f0dc75d67a144cbfe051fc4dcfd4e0c
Ahmad Mario <riotheassassin@gmail.com>
2023-12-25 14:31:11 +0700

    Adding menu scenario and some students object.

--- Main.java
+++ Main.java
@@ -1,16 +1,25 @@
 import java.util.*;
 public class Main {
     public static void main(String[] args) {
+        Student ahmad = new Student("Ahmad", "Yahya");
+        Student ihsan = new Student("Ihsan", "Alawy");
+        Student noor = new Student("Noor", "Yahya");
+        Student yazed = new Student("Yazed", "Nouman");
+
         Scanner input = new Scanner(System.in);
         String guestName, studentsName;
         System.out.println("Welcome to Guest Receptionist");
-        System.out.print("Please insert your name: ");
-        guestName = input.nextLine();
-        System.out.println("Next, please insert your child name: ");
-        studentsName = input.nextLine();
-        System.out.println(guestName);
-        System.out.println(studentsName);
-        input.close();
-        //comment
+        System.out.println("Input 1 to continue.\nInput 0 to entering Administator.");
+        int menu = input.nextInt();
+        input.nextLine();
+        if (menu == 1) {    
+            System.out.println("Please insert your name: ");
+            guestName = input.nextLine();
+            System.out.println("Next, please insert your child name: ");
+            studentsName = input.nextLine();
+            System.out.println("Guest Name" + guestName);
+            System.out.println("Child Name" + studentsName);
+            input.close();
+        }
     }
 }
